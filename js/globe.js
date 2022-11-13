23a39dec058b4136d0f45fc1e81e0299034abdaf
Umang Bhalodiya <umangbhalodiya660@gmail.com>
2022-11-13 11:21:43 +0530

    earths deployed

---
+++ js/globe.js
@@ -0,0 +1,304 @@
+var DAT = DAT || {};
+
+DAT.Globe = function(container, opts) {
+  opts = opts || {};
+  
+  var colorFn = opts.colorFn || function(x) {
+    var c = new THREE.Color();
+    c.setHSL( ( 0.6 - ( x * 0.5 ) ), 1.0, 0.5 );
+    return c;
+  };
+
+  var imgDir = './';
+
+  var Shaders = {
+    'earth' : {
+      uniforms: {
+        'texture': { type: 't', value: null }
+      },
+      vertexShader: [
+        'varying vec3 vNormal;',
+        'varying vec2 vUv;',
+        'void main() {',
+          'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+          'vNormal = normalize( normalMatrix * normal );',
+          'vUv = uv;',
+        '}'
+      ].join('\n'),
+      fragmentShader: [
+        'uniform sampler2D texture;',
+        'varying vec3 vNormal;',
+        'varying vec2 vUv;',
+        'void main() {',
+          'vec3 diffuse = texture2D( texture, vUv ).xyz;',
+          'float intensity = 1.05 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
+          'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 3.0 );',
+          'gl_FragColor = vec4( diffuse + atmosphere, 1.0 );',
+        '}'
+      ].join('\n')
+    },
+    'atmosphere' : {
+      uniforms: {},
+      vertexShader: [
+        'varying vec3 vNormal;',
+        'void main() {',
+          'vNormal = normalize( normalMatrix * normal );',
+          'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+        '}'
+      ].join('\n'),
+      fragmentShader: [
+        'varying vec3 vNormal;',
+        'void main() {',
+          'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 12.0 );',
+          'gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 ) * intensity;',
+        '}'
+      ].join('\n')
+    }
+  };
+
+  var camera, scene, renderer, w, h;
+  var mesh, atmosphere, point;
+
+  var overRenderer;
+
+  var curZoomSpeed = 0;
+  var zoomSpeed = 50;
+
+  var mouse = { x: 0, y: 0 }, mouseOnDown = { x: 0, y: 0 };
+  var rotation = { x: 0, y: 0 },
+      target = { x: Math.PI*3/2, y: Math.PI / 6.0 },
+      targetOnDown = { x: 0, y: 0 };
+
+  var distance = 100000, distanceTarget = 100000;
+  var padding = 40;
+  var PI_HALF = Math.PI / 2;
+
+  var ROTATIONSPEED = 0.002;
+  var k = ROTATIONSPEED;
+  var f = false;
+
+  function init() {
+
+    container.style.color = '#fff';
+    container.style.font = '13px/20px Arial, sans-serif';
+
+    var shader, uniforms, material;
+    w = container.offsetWidth || window.innerWidth;
+    h = container.offsetHeight || window.innerHeight;
+
+    camera = new THREE.PerspectiveCamera(30, w / h, 1, 10000);
+    camera.position.z = distance;
+
+    scene = new THREE.Scene();
+
+    var geometry = new THREE.SphereGeometry(200, 40, 30);
+
+    shader = Shaders['earth'];
+    uniforms = THREE.UniformsUtils.clone(shader.uniforms);
+    uniforms['texture'].value = THREE.ImageUtils.loadTexture(opts.textureImg);
+
+    material = new THREE.ShaderMaterial({
+      uniforms: uniforms,
+      vertexShader: shader.vertexShader,
+      fragmentShader: shader.fragmentShader
+    });
+
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.rotation.y = Math.PI;
+    scene.add(mesh);
+
+    shader = Shaders['atmosphere'];
+    uniforms = THREE.UniformsUtils.clone(shader.uniforms);
+
+    material = new THREE.ShaderMaterial({
+      uniforms: uniforms,
+      vertexShader: shader.vertexShader,
+      fragmentShader: shader.fragmentShader,
+      side: THREE.BackSide,
+      blending: THREE.AdditiveBlending,
+      transparent: true
+    });
+
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.scale.set( 1.1, 1.1, 1.1 );
+    scene.add(mesh);
+
+    geometry = new THREE.BoxGeometry(0.75, 0.75, 1);
+    geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0,0,-0.5));
+
+    point = new THREE.Mesh(geometry);
+
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(w, h);
+
+    renderer.domElement.style.position = 'absolute';
+
+    container.appendChild(renderer.domElement);
+
+    // Listeners
+    container.addEventListener('mousedown', onMouseDown, false);
+    container.addEventListener('mousewheel', onMouseWheel, false);
+    document.addEventListener('keydown', onDocumentKeyDown, false);
+    window.addEventListener('resize', onWindowResize, false);
+    container.addEventListener('mouseover', function() {
+      overRenderer = true;
+    }, false);
+    container.addEventListener('mouseout', function() {
+      overRenderer = false;
+    }, false);
+  }
+
+  function onMouseDown(event) {
+    event.preventDefault();
+
+    k = 0;
+    f = true;
+
+    container.addEventListener('mousemove', onMouseMove, false);
+    container.addEventListener('mouseup', onMouseUp, false);
+    container.addEventListener('mouseout', onMouseOut, false);
+
+    target.y = rotation.y;
+
+    mouseOnDown.x = - event.clientX;
+    mouseOnDown.y = event.clientY;
+
+    targetOnDown.x = target.x;
+    targetOnDown.y = target.y;
+
+    container.style.cursor = 'move';
+  }
+
+  function onMouseMove(event) {
+    mouse.x = - event.clientX;
+    mouse.y = event.clientY;
+
+    var zoomDamp = distance/1000;
+
+    target.x = targetOnDown.x + (mouse.x - mouseOnDown.x) * 0.005 * zoomDamp;
+    target.y = targetOnDown.y + (mouse.y - mouseOnDown.y) * 0.005 * zoomDamp;
+
+    target.y = target.y > PI_HALF ? PI_HALF : target.y;
+    target.y = target.y < - PI_HALF ? - PI_HALF : target.y;
+  }
+
+  function onMouseUp(event) {
+    k = ROTATIONSPEED;
+    f = false;
+
+    container.removeEventListener('mousemove', onMouseMove, false);
+    container.removeEventListener('mouseup', onMouseUp, false);
+    container.removeEventListener('mouseout', onMouseOut, false);
+    container.style.cursor = 'auto';
+  }
+
+  function onMouseOut(event) {
+    k = ROTATIONSPEED;
+    f = false;
+
+    container.removeEventListener('mousemove', onMouseMove, false);
+    container.removeEventListener('mouseup', onMouseUp, false);
+    container.removeEventListener('mouseout', onMouseOut, false);
+  }
+
+  function onMouseWheel(event) {
+    event.preventDefault();
+    if (overRenderer) {
+      zoom(event.wheelDeltaY * 0.3);
+    }
+    return false;
+  }
+
+  function onDocumentKeyDown(event) {
+    switch (event.keyCode) {
+      case 38:
+        zoom(100);
+        event.preventDefault();
+        break;
+      case 40:
+        zoom(-100);
+        event.preventDefault();
+        break;
+    }
+  }
+
+  function onWindowResize( event ) {
+    camera.aspect = container.offsetWidth / container.offsetHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( container.offsetWidth, container.offsetHeight );
+  }
+
+  function zoom(delta) {
+    distanceTarget -= delta;
+    distanceTarget = distanceTarget > 1000 ? 1000 : distanceTarget;
+    distanceTarget = distanceTarget < 350 ? 350 : distanceTarget;
+  }
+
+  function animate() {
+    requestAnimationFrame(animate);
+    render();
+  }
+
+  function render() {
+    zoom(curZoomSpeed);
+
+    target.x -= k;
+
+    rotation.x += (target.x - rotation.x) * 0.2;
+
+    if (f == true){
+      rotation.y += (target.y - rotation.y) * 0.2;}
+    if (f == false){
+      target.y = Math.PI / 5.0;
+      rotation.y += (target.y - rotation.y) * 0.02;
+    };
+
+    distance += (distanceTarget - distance) * 0.3;
+
+    camera.position.x = distance * Math.sin(rotation.x) * Math.cos(rotation.y);
+    camera.position.y = distance * Math.sin(rotation.y);
+    camera.position.z = distance * Math.cos(rotation.x) * Math.cos(rotation.y);
+
+    camera.lookAt(mesh.position);
+
+    renderer.render(scene, camera);
+  }
+
+  init();
+
+  this.animate = animate;
+
+  this.__defineGetter__('time', function() {
+    return this._time || 0;
+  });
+
+  this.__defineSetter__('time', function(t) {
+    var validMorphs = [];
+    var morphDict = this.points.morphTargetDictionary;
+    for(var k in morphDict) {
+      if(k.indexOf('morphPadding') < 0) {
+        validMorphs.push(morphDict[k]);
+      }
+    }
+    validMorphs.sort();
+    var l = validMorphs.length-1;
+    var scaledt = t*l+1;
+    var index = Math.floor(scaledt);
+    for (i=0;i<validMorphs.length;i++) {
+      this.points.morphTargetInfluences[validMorphs[i]] = 0;
+    }
+    var lastIndex = index - 1;
+    var leftover = scaledt - index;
+    if (lastIndex >= 0) {
+      this.points.morphTargetInfluences[lastIndex] = 1 - leftover;
+    }
+    this.points.morphTargetInfluences[index] = leftover;
+    this._time = t;
+  });
+
+  this.renderer = renderer;
+  this.scene = scene;
+
+  return this;
+
+};
---
+++ js/index.js
@@ -0,0 +1,103 @@
+'use strict'
+
+var myApp = angular.module('myApp', []);
+
+myApp.controller('globeGL', function($scope, $http) {
+
+    $scope.globeGL = {};
+
+    // Variables
+    $scope.globeGL.selectedTexture = 'Land Surface Temperature';
+    $scope.globeGL.selectedTextureImg = './textures/land-surface-temperature.jpeg';
+    $scope.globeGL.selectedDescription = 'Land surface temperature is how hot the ground feels to the touch. If you want to know whether temperatures at some place at a specific time of year are unusually warm or cold, you need to compare them to the average temperatures for that place over many years. These maps show the average weekly or monthly daytime land surface temperatures for 2001-2010.';
+
+    $scope.globeGL.textures = [
+        {
+            name: 'Land Surface Temperature',
+            icon: './icons/temperature.svg',
+            textureImg: './textures/land-surface-temperature.jpeg',
+            description: 'Land surface temperature is how hot the ground feels to the touch. If you want to know whether temperatures at some place at a specific time of year are unusually warm or cold, you need to compare them to the average temperatures for that place over many years. These maps show the average weekly or monthly daytime land surface temperatures for 2001-2010.'
+        },
+        {
+            name: 'Cloud Fraction',
+            icon: './icons/clouds.svg',
+            textureImg: './textures/cloud-fraction.jpeg',
+            description: 'Looking at Earth from outer space, clouds are easy to spot. Clouds are draped all around Earth like bright white decorations. Clouds are important to scientists because they reflect the Sun\'s light back to space and give shade to the surface. They also bring rain, which is important because all plants and animals need freshwater to live. These maps made from NASA satellite observations show how much of Earth\'s surface is covered by clouds for a given day, or over a span of days.'
+        },
+        {
+            name: 'Population Density',
+            icon: './icons/population-density.svg',
+            textureImg: './textures/population-density.jpeg',
+            description: 'This map shows how many people live in different areas on Earth. The map is divided into numerous small boxes, called "grids." Each grid box is about 1 kilometer long by one kilometer wide, and it is color coded to show how many people live there. Lighter areas have fewer people. The red dots scattered across most countries show cities, where many people live in a small area.'
+        },
+        {
+            name: 'Leaf Area Index',
+            icon: './icons/leaf-area-index.svg',
+            textureImg: './textures/leaf-area-index.jpeg',
+            description: 'Have you ever flown in a plane over a forest, or seen a picture of a forest canopy taken from above? If so, you probably noticed the forest canopy was colored shades of dark green. The trees\' and plants\' leaves give the forest its lush green appearance. The more leaves there are in a forested area, the greener the tree canopy. Have you ever wondered how many leaves there are in a forest? Today, scientists use NASA satellites to map leaf area index — images processed to show how much of an area is covered by leaves. For example, a leaf area index of one means the area is entirely covered by one layer of leaves. Knowing the total area covered by leaves helps scientists monitor how much water, carbon, and energy the trees and plants are exchanging with the air above and the ground below.'
+        },
+        {
+            name: 'Vegetation Index',
+            icon: './icons/vegetation-index.svg',
+            textureImg: './textures/vegetation-index.jpeg',
+            description: 'Our lives depend upon plants and trees. They feed us and give us clothes. They absorb carbon dioxide and give off oxygen we need to breathe. Plants even provide many of our medicines and building materials. So when the plants and trees around us change, these changes can affect our health, our environment, and our economy. For these reasons, and more, scientists monitor plant life around the world. Today, scientists use NASA satellites to map the "greenness" of all Earth\'s lands. These vegetation index maps show where and how much green leaf vegetation was growing for the time period shown.'
+        },
+        {
+            name: 'Albedo',
+            icon: './icons/albedo.svg',
+            textureImg: './textures/albedo.jpeg',
+            description: 'When sunlight reaches the Earth’s surface, some of it is absorbed and some is reflected. The relative amount (ratio) of light that a surface reflects compared to the total sunlight that falls on it is called albedo. Surfaces that reflect a lot of the light falling on them are bright, and they have a high albedo. Surfaces that don’t reflect much light are dark, and they have a low albedo. Snow has a high a albedo, and forests have a low albedo.'
+        },
+        {
+            name: 'Sea Surface Temperature',
+            icon: './icons/sea-surface-temperature.svg',
+            textureImg: './textures/sea-surface-temperature.jpeg',
+            description: 'Sea surface temperature is the temperature of the top millimeter of the ocean\'s surface. Sea surface temperatures influence weather, including hurricanes, as well as plant and animal life in the ocean. Like Earth\'s land surface, sea surface temperatures are warmer near the equator and colder near the poles. Currents like giant rivers move warm and cold water around the world\'s oceans. Some of these currents flow on the surface, and they are obvious in sea surface temperature images. Warm ocean waters help form clouds and affect weather patterns. The sea\'s surface temperature is also correlated to the availability of tiny ocean plants, called phytoplankton. For all of these reasons scientists monitor the sea\'s surface temperature. These maps show satellite measurements of the sea\'s surface temperature for a given day, or for a span of days.'
+        },
+        {
+            name: 'Chlorophyll Concentration',
+            icon: './icons/chlorophyll-concentration.svg',
+            textureImg: './textures/chlorophyll-concentration.jpeg',
+            description: 'This map shows where tiny, floating plants live in the ocean. These plants, called phytoplankton, are an important part of the ocean\'s food chain because many animals (such as small fish and whales) feed on them. Scientists can learn a lot about the ocean by observing where and when phytoplankton grow in large numbers. Scientists use satellites to measure how much phytoplankton are growing in the ocean by observing the color of the light reflected from the shallow depths of the water. Phytoplankton contain a photosynthetic pigment called chlorophyll that lends them a greenish color. When phytoplankton grow in large numbers they make the ocean appear greenish. These maps made from satellite observations show where and how much phytoplankton were growing on a given day, or over a span of days. The black areas show where the satellite could not measure phytoplankton.'
+        }
+    ];
+
+    // Functions
+    $scope.globeGL.changeTexture = changeTexture;
+
+    function changeTexture(texture) {
+        $scope.globeGL.selectedTexture = texture.name
+        $scope.globeGL.selectedTextureImg = texture.textureImg;
+        $scope.globeGL.selectedDescription = texture.description;
+
+        paint();
+    }
+
+    function paint() {
+        var container = document.getElementById('container');
+        var globe = new DAT.Globe(container, {
+            textureImg: $scope.globeGL.selectedTextureImg
+        });
+    
+        TWEEN.start();
+        
+        globe.animate();
+        document.body.style.backgroundImage = 'none';
+    }
+
+
+    function initialize() {
+        if(!Detector.webgl){
+            Detector.addGetWebGLMessage();
+        } else {
+            paint();
+        }
+    }
+
+    // window.addEventListener('resize', function() {
+    //     // TODO keep selected image
+    //     initialize();
+    // });
+
+    initialize();
+});
